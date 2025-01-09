43bcba8ac15b603f569e2b6d287f3a349077ec6f
Sergiu Chilat <chilatsergiu@gmail.com>
2025-01-09 12:03:07 +0200

    Add homework

---
+++ examples/2. Connecting JS file/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.style.backgroundColor = 'blue'
+
+    document.getElementById('greenBgButton').addEventListener('click', () => {
+        document.body.style.backgroundColor = 'green'
+    })
+})
