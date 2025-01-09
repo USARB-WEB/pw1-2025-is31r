255e5be096707fa42c02a152fdbc5f86f6f8ab9e
Sergiu Chilat <chilatsergiu@gmail.com>
2025-01-09 12:31:06 +0200

    JS Basic syntax example

---
+++ examples/3. Basic Syntax/app.js
@@ -0,0 +1,120 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    /*
+    let x = 2;
+    let y = 3;
+
+    console.log(`${x}+${y}=`, x + y)
+    console.log(`${x}-${y}=`, x - y)
+    console.log(`${x}*${y}=`, x * y)
+    console.log(`${x}/${y}=`, x / y)
+    console.log(`${x}%${y}=`, x % y)
+
+    console.log(0.1 + 0.2 === 0.3)
+    console.log(0.1 + 0.3 === 0.4)
+
+    const PI = 3.14
+
+    const name = prompt('Hello. I am a JS code. Whats is your name?')
+    alert(`Hello ${name}`)
+
+    const knowJS = confirm('Do you know JS?')
+
+    if(knowJS){
+        alert('Great!')
+    } else {
+        document.body.style.backgroundColor = 'red'
+    }
+
+    */
+
+    // console.log(2 == 2.0);
+    // console.log(2 === 2.0);
+    // console.log(2 == "2");
+    // console.log(2 === "2");
+
+
+    // const digit = +prompt('Inpuit a digit');
+
+    // console.log(typeof digit)
+
+    // if (digit == 0) {
+    //     alert('Zero')
+    // } else if (digit == 1) {
+    //     alert('One')
+    // } else if (digit == 2) {
+    //     alert('Two')
+    // } else if (digit == 3) {
+    //     alert('Three')
+    // } else if (digit == 4) {
+    //     alert('Four')
+    // } else if (digit == 5) {
+    //     alert('Five')
+    // } else if (digit == 6) {
+    //     alert('Six')
+    // } else if (digit == 7) {
+    //     alert('Seven')
+    // } else if (digit == 8) {
+    //     alert('Eight')
+    // } else if (digit == 9) {
+    //     alert('Nine')
+    // } else {
+    //     alert('Not a digit')
+    // }
+
+    // switch (digit) {
+    //     case 0:
+    //         alert('Zero')
+    //         break;
+    //     case 1:
+    //         alert('One')
+    //         break;
+    //     case 2:
+    //         alert('Two')
+    //         break;
+    //     case 3:
+    //         alert('Three')
+    //         break;
+    //     case 4:
+    //         alert('Four')
+    //         break;
+    //     case 5:
+    //         alert('Five')
+    //         break;
+    //     case 6:
+    //         alert('Six')
+    //         break;
+    //     case 7:
+    //         alert('Seven')
+    //         break;
+    //     case 8:
+    //         alert('Eight')
+    //         break;
+    //     case 9:
+    //         alert('Nine')
+    //         break;
+    //     default:
+    //         alert('Not a digit')
+    // }
+
+    console.log('For')
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log('While')
+    let i = 0;
+    while (i < 5) {
+        console.log(i);
+        i++;
+    }
+
+    console.log('DO While')
+    let a = 4;
+    let b = 4;
+    do {
+        console.log(a);
+        a++;
+    } while (a < b)
+
+});
