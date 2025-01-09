e2bf0ffab0211eae350474a56dddaa3cb4b3a4cd
Sergiu Chilat <chilatsergiu@gmail.com>
2025-01-09 13:03:57 +0200

    JS Array basics

---
+++ examples/5. Arrays/app.js
@@ -0,0 +1,32 @@
+const a1 = 4;
+const a2 = 8;
+const a3 = -2;
+const a4 = 3;
+const a5 = 0;
+const a6 = 1;
+const a7 = 14;
+
+const sum = a1 + a2 + a3 + a4 + a5 + a6 + a7;
+
+console.log(sum);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const square = (n) => n ** 2;
+
+console.log(numbers.map((n) => square(n)))
+
+
+
+/* Off top*/
+console.log(typeof numbers)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(0 == null)
+console.log(0 === null)
+console.log(0 >= null)
+console.log(0 <= null)
+console.log(0 != null)
+console.log(0 !== null)
+console.log(0 >= undefined)
+console.log(0 <= undefined)
