14b729f64556d187a0cddd3c785ca5d43877bf18
sergiuchilat <chilatsergiu@gmail.com>
2025-01-10 10:09:45 +0200

    Delete 2 old folders with examples

--- 2. Connecting JS file/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.style.backgroundColor = 'blue'
-
-    document.getElementById('greenBgButton').addEventListener('click', () => {
-        document.body.style.backgroundColor = 'green'
-    })
-})
